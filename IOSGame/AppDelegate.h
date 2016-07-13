11a17449739b4e43cecdae07e642f8f7bbd101ff
Attila-Varga <Death@attilas-macbook-pro.local>
2016-07-13 20:08:15 +0100

    Initial Commit

---
+++ IOSGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSGame
+//
+//  Created by Attila Varga on 13/07/2016.
+//  Copyright © 2016 Attila Varga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  IOSGame
+//
+//  Created by Attila Varga on 13/07/2016.
+//  Copyright © 2016 Attila Varga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
